f823c17566ef1d11a9b404765dd3934c8cb7a750
Hrompic <semenxx@gmail.com>
2019-02-13 19:13:40 +0200

    modified:   1.c 	modified:   Makefile

--- c/pthread/1.c
+++ c/pthread/1.c
@@ -13,8 +13,8 @@ void* func(void *att)
 //	n++;
 	double t=.0;
 	d =&t;
-	for(int i=0; i<(1<<31); i++)
-		t = sqrt(pow(i*i, (double)1/2));
+	for(unsigned long long i=0; i<(1L<<63L); i++)
+		t =+ sqrt(pow(i*i, (double)1/.2));
 	//return (void *)&d;
 	return &t;
 }
@@ -23,4 +23,7 @@ int main()
 	pthread_t th[N];
 	for(int i=0; i<N; i++)
 		pthread_create(&th[i], NULL, func, NULL);
+	for(int i=0; i<N; i++)
+		pthread_join(th[i], NULL);
+
 }
