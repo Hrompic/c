472887bd9531ed5894c8ee9b631ff554e734c585
Hrompic <semenxx@gmail.com>
2019-06-24 21:14:48 +0300

    add mutex

--- c/mutex/1.c
+++ c/mutex/1.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 #include <pthread.h>
-#define N 1<<7
+#define N 1<<13
 pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
+int counter=0;
 void* f(void*);
 
 int main()
 {
-	printf("%d\n", N);
 	pthread_t th1, th2;
+	printf("%d\n", N);
+
 	pthread_create(&th1, NULL, &f, NULL);
 	pthread_create(&th2, NULL, &f, NULL);
+
 	pthread_join(th1, NULL);
 	pthread_join(th2, NULL);
+
 	return 0;
 }
+
 void* f(void* ptr)
 {
-	int val=0, counter=0;
+	int val=0;
 	for(int i=0; i<N; i++)
 	{
 		val = counter;
+
 		pthread_mutex_lock(&mtx);
-		printf("%d: %d\n", pthread_self(), val + 1);
-		counter = val + 1;
+			printf("%d: %d\n", pthread_self(), val + 1);
+			counter = val + 1;
 		pthread_mutex_unlock(&mtx);
 	}
+
 	return NULL;
 }
