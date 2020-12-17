2b5de2e1e2944b411203fe178628e42f826a4bce
Hrompic <semenxx@gmail.com>
2020-12-17 10:22:57 +0200

    Zero error

---
+++ sortingAlgoritm/selectionSort/1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define sz 100
+#define swap(a, b) do{a^=b; b^=a; a^=b;} while(0)
+void dump(int *mass)
+{
+    for(int i=0; i<sz; i++)
+        printf("%d\t", mass[i]);
+    printf("\n");
+}
+int main()
+{
+    srand(time(NULL));
+    int mass[sz];
+    for(int i=0; i<sz; i++)
+    {
+        mass[i] = rand()%sz;
+    }
+    dump(mass);
+    int min;
+    for(int i=0; i<sz-1; i++)
+    {
+        min=i;
+        for(int j = i+1; j<sz; j++)
+            if(mass[min] > mass[j])
+                min = j;
+            swap(mass[i], mass[min]);
+    }
+    dump(mass);
+}
