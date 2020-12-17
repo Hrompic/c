98b2f161ec8091c35bec6ea21779fe6f9b900d12
Hrompic <semenxx@gmail.com>
2020-12-17 23:34:00 +0200

    Fix zeroes

--- sortingAlgoritm/selectionSort/1.c
+++ sortingAlgoritm/selectionSort/1.c
@@ -19,13 +19,16 @@ int main()
     }
     dump(mass);
     int min;
-    for(int i=0; i<sz-1; i++)
+    for(int i=0; i < sz-1; i++)
     {
         min=i;
         for(int j = i+1; j<sz; j++)
-            if(mass[min] > mass[j])
+		{
+            if(mass[min] >= mass[j])
                 min = j;
-            swap(mass[i], mass[min]);
-    }
+		}
+		if(i!=min)
+			swap(mass[i], mass[min]);
+	}
     dump(mass);
 }
